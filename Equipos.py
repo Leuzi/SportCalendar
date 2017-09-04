0c7db4d4169257ffb623834a99c031f4bd9872ec
Ramon Izuel Rios <izuelrios@gmail.com>
2017-09-04 15:19:13 +0200

    old version working

--- Equipos.py
+++ Equipos.py
@@ -1,18 +1,9 @@
+# -*- coding: utf-8 -*-
 class Equipo():
 	def __init__(self,nombre,iniciales,indice):
 		self.nombre = nombre
-		self.indice = int(indice)
 		self.iniciales = iniciales
-		self.distancias = dict()
-	
+		self.indice = int(indice)
+		
 	def __str__(self):
-		result = self.nombre + '\n'
-		return result
-	
-	def ponDistancias(self,equipos,distancias):
-		for equipo in equipos:		
-			self.distancias[equipo] = distancias[self.indice][equipo.indice]
-	
-	__repr__ = __str__
-	
-	
+		return self.nombre
--- main.py
+++ main.py
@@ -2,42 +2,81 @@
 from Equipos import Equipo
 from Calendario import Calendario
 import numpy
-from itertools import combinations
+import itertools
 import xlsxwriter
 
 def cargaDatos():
 
 	#Abrimos el archivo que contiene los datos
 
+	ciudades = cargaCiudades('datos')
 	matriz = cargaMatriz('costes')
-	ciudades = cargaEquipos('datos',matriz)
-	
 	return ciudades,matriz
 
-def cargaEquipos(nombre,matriz):
+def cargaCiudades(nombre):
 
-	equipos = []
-	archivo = open(nombre,"r")
+	ciudades = []
+	archivo = open(nombre,"r", encoding='utf-8')
 
-	for i in range(8):
+	for i in range(6):
 		nombre,iniciales,indice= archivo.readline().split('\t')
-		equipos.append(Equipo(nombre,iniciales,indice))
-	return equipos
+		print(nombre)
+		ciudades.append(Equipo(nombre,iniciales,indice))
+		
+	return ciudades
 
 def cargaMatriz(nombre):
-	distancias = numpy.loadtxt(nombre, delimiter="\t")
-	return distancias
+	matriz = numpy.loadtxt(nombre, delimiter="\t")
+	return matriz
 			
+def all_pairs(lst):
+    if len(lst) < 2:
+        yield lst
+        return
+    a = lst[0]
+    for i in range(1,len(lst)):
+        pair = (a,lst[i])
+        for rest in all_pairs(lst[1:i]+lst[i+1:]):
+            yield [pair] + rest
+
 def main():
-	equipos,distancias = cargaDatos()
-	
-	for equipo in equipos:
-		equipo.ponDistancias(equipos,distancias)
+	ciudades,matriz = cargaDatos()
+
+	i = 0
+
+	calendarios=[]
 		
-	for i in range(3,8):
-		set = list(combinations(equipos,i))
-		for equipos in set:
-			Calendario(equipos)
+	for equipos in all_pairs(ciudades):
+		lst = list(itertools.product([0, 1], repeat=12))
+		for creador in lst:
+			calendario = Calendario(creador,equipos,i)			
+			if calendario.es_valido():				
+				calendario.generar_resultados(matriz)				
+				calendarios.append(calendario)
+				i = i+1
+
+	calendarios.sort(key=lambda x: x.total)
+
+	workbook = xlsxwriter.Workbook('total.xlsx')	
+		
+	i = 1
+	for calendario in calendarios:
+		nombre = 'Opcion '+ str(i)
+		
+		for equipos in calendario.partidos:
+			if ciudades[0] in equipos and ciudades[5] in equipos:
+				for equipos1 in calendario.partidos:
+					if ciudades[1] in equipos1 and ciudades[4] in equipos1:
+						for equipos2 in calendario.partidos:
+							if ciudades[2] in equipos2 and ciudades[3] in equipos2:
+								nombre = nombre + 'Respeta ranking'
+		worksheet = workbook.add_worksheet(nombre)
+		calendario.imprime_resultado(workbook,worksheet,ciudades)
+		i = i+1
+	
+	workbook.close()
+	print('Total calendario:'+str(i))
+	
 		
 if __name__ == "__main__":
 
