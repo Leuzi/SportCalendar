e05153d08429bec7ebec6e6e7ddbbb30400dec22
Ramon Izuel Rios <izuelrios@gmail.com>
2019-09-13 03:29:46 +0200

    New

--- Calendario.py
+++ Calendario.py
@@ -6,50 +6,161 @@ import xlsxwriter
 class Calendario():
 	def __init__(self,grupos,costes):
 		self.grupos = grupos
+		self.equiposGrupoA = grupos[0]
+		self.equiposGrupoB = grupos[1]
 		self.costes = costes
-
-	def generar_calendario(self):
-		matriz = numpy.zeros((4,4),dtype=numpy.int)
-		calendario = [
-		[0,0,0,1],
-		[1,0,0,0],
-		[1,1,0,0],
-		[0,1,1,0]
+		self.distanciasEquipos = []
+		self.matriz = []
+		self.total = 0
+		self.media = 0
+		self.desviacion = 0
+		
+		self.jornadas = [
+		[0,1,3,3,0,2,1,3],
+		[1,2,3,4,1,3,2,4],
+		[2,3,4,0,2,4,3,0],
+		[3,4,0,1,3,0,4,1],
+		[4,0,1,2,4,1,0,2]
 		]
 		
+		self.descansa = [4,0,1,2,3]
+		self.organiza = [0,1,2,3,4]
+
+	def generar_calendario(self):
+		matriz = numpy.zeros((5,5),dtype=numpy.int)
+		
+		
+		
+		matriz = numpy.zeros((10,10),dtype=numpy.int)
 		for grupo in self.grupos:
-			matriz = numpy.zeros((4,4),dtype=numpy.int)
-			print(grupo.equipos)
-			for i in range(4):
-				for j in range(4):
-					matriz[i][j] = calendario[i][j] * self.costes[grupo.equipos[i].indice][grupo.equipos[j].indice]
-			print(matriz)
-			grupo.costeGrupo = matriz
-			distanciasEquipos = numpy.zeros((4,1),dtype=numpy.int)
+			for i in range(len(self.jornadas)):
+				equipoDescansa = grupo[self.descansa[i]]
+				equipoOrganiza = grupo[self.organiza[i]]				
+								
+				desplazamientos = []
+				for equipo in grupo:
+					if(equipo.indice != equipoDescansa.indice):
+						if(equipo.indice != equipoOrganiza.indice):
+							desplazamientos.append(equipo)
+				
+				"""		
+				print("Jornada " + str(i+1))
+				print("self.organiza " + equipoOrganiza.nombre)
+				print("self.descansa " + equipoDescansa.nombre)
+				"""
+				for desplazamiento in desplazamientos:
+					matriz[desplazamiento.indice,equipoOrganiza.indice] = self.costes[desplazamiento.indice,equipoOrganiza.indice]
+		"""
+		print(matriz)
+		"""
+		distanciasEquipos = numpy.zeros((10,1),dtype=numpy.int)	
+		"""
+		print(self.grupos)
+		"""
+		
+		for grupo in self.grupos:
+			for equipo in grupo:
+				"""
+				print(equipo)
+				"""	
+				for i  in range(len(matriz)):
+					distanciasEquipos[equipo.indice,0] = distanciasEquipos[equipo.indice,0] + matriz[equipo.indice,i]						
+		print(distanciasEquipos)
+		desviacion = 0
+		media = 0
+		total = 0
+		distanciasEquiposConEtiqueta = {}
+		
+				
+		total = sum(distanciasEquipos)
+		"""
+		print(distanciasEquipos)
+		print(total)
+		"""
+		media = sum(distanciasEquipos) / float(len(distanciasEquipos))
+		for distancia in distanciasEquipos:
+			desviacion = desviacion + (distancia-media)*(distancia-media)
+
+		desviacion = math.sqrt(desviacion/10)
+		
+		
+		self.matriz = matriz
+		self.distanciasEquipos = distanciasEquipos
+		self.desviacion = desviacion
+		self.media = media
+		self.total = total
+
+	def imprime_resultado(self,workbook,worksheet):
+		
+		worksheet.set_column(0, 0, 22)	
+		worksheet.set_column(1, 1, 18)	
+		worksheet.set_column(2, 2, 18)	
+		worksheet.set_column(3, 3, 18)	
+		worksheet.set_column(4, 4, 18)	
+		worksheet.set_column(5, 5, 18)	
+		worksheet.set_column(6, 6, 18)	
+		worksheet.set_column(7, 7, 18)	
+		
+
+		bold = workbook.add_format({'bold': True})
+		bold.set_font_color('white')
+		bold.set_font_size(14)
+		bold.set_bg_color('00cc00')
+		
+		i=0
+		worksheet.write(1,0,'GrupoA',bold)	
+		
+		for equipo in self.equiposGrupoA:
+			worksheet.write(2+i,0,equipo.nombre,bold)
+				
+			i= i+1
+		
+		i=0
+		worksheet.write(1,4,'GrupoB',bold)	
+		
+		for equipo in self.equiposGrupoB:
+			worksheet.write(2+i,4,equipo.nombre,bold)
+				
+			i= i+1
+		
+		filaActual = 8
+			
+		title = workbook.add_format({'bold':True})
+		title.set_bg_color('000000')
+		title.set_font_size(11)
+		title.set_font_color('white')
+		
+		j = 0
+		
+		for grupo in self.grupos:
+			for i in range(len(self.jornadas)):
+				equipoDescansa = grupo[self.descansa[i]]
+				equipoOrganiza = grupo[self.organiza[i]]				
+				filaActual = filaActual +1
+				worksheet.write(filaActual, 0+j, "Jornada" + str(i+1))
+				worksheet.write(filaActual, 1+j, "Organiza " + equipoOrganiza.nombre)
+				worksheet.write(filaActual, 2+j, "Descansa " + equipoDescansa.nombre)
+				
+				for equipo1,equipo2 in zip(self.jornadas[i][::2], self.jornadas[i][1::2]):
+					filaActual = filaActual +1
+					worksheet.write(filaActual, 0+j, grupo[equipo1].nombre)
+					worksheet.write(filaActual, 1+j, grupo[equipo2].nombre)
 					
+			print(self.distanciasEquipos)
+			filaActual = 8
 		
-			desviacion = 0
-			media = 0
-			total = 0
-			distanciasEquiposConEtiqueta = {}
-			
-			for i in range(4):
-				for j in range(4):			
-					distanciasEquipos[grupo.equipos[i].indice] = distanciasEquipos[grupo.equipos[i].indice] + matriz[grupo.equipos[i].indice][grupo.equipos[j].indice]
-					distanciasEquiposConEtiqueta.update()
-			total = sum(distanciasEquipos)
-			print(distanciasEquipos)
-			print(total)
-
-			media = sum(distanciasEquipos) / float(len(distanciasEquipos))
-			for distancia in distanciasEquipos:
-				desviacion = desviacion + (distancia-media)*(distancia-media)
-
-			desviacion = math.sqrt(desviacion/9)
-			
-			grupo.setCoste(matriz,distanciasEquipos,desviacion,media,total)
-	
-
 		
-		return matriz
-
+			for i in range(len(grupo)):
+				print(str(i))
+				worksheet.write(filaActual+i+j,8, grupo[i].nombre)
+				worksheet.write(filaActual+i+j, 9, str(self.distanciasEquipos[grupo[i].indice]) + " kms")
+			j = j +5
+		i=0
+		
+		filaActual = 0
+		worksheet.write(filaActual,7,'Media por equipo',bold)
+		worksheet.write(filaActual,8,str(self.media)+' kms')	
+		worksheet.write(filaActual+1,7,'Klm entre todos',bold)
+		worksheet.write(filaActual+1,8,str(self.total)+' kms')	
+		worksheet.write(filaActual+2,7,'Desviacion',bold)
+		worksheet.write(filaActual+2,8,str(self.desviacion)+' kms')
--- main.py
+++ main.py
@@ -6,13 +6,14 @@ import numpy
 import itertools
 import xlsxwriter
 from collections import Counter
+import sys
 
-def cargaDatos():
+def cargaDatos(datos,costes):
 
 	#Abrimos el archivo que contiene los datos
 
-	equipos = cargaEquipos('datos')
-	matriz = cargaMatriz('costes')
+	equipos = cargaEquipos(datos)
+	matriz = cargaMatriz(costes)
 	return equipos,matriz
 
 def cargaEquipos(nombre):
@@ -20,7 +21,7 @@ def cargaEquipos(nombre):
 	equipos = []
 	archivo = open(nombre,"r", encoding='utf-8')
 
-	for i in range(4):
+	for i in range(10):
 		nombre,iniciales,ciudad,indice= archivo.readline().split('\t')
 		equipos.append(Equipo(nombre,iniciales,ciudad,indice))
 		
@@ -29,50 +30,70 @@ def cargaEquipos(nombre):
 def cargaMatriz(nombre):
 	matriz = numpy.loadtxt(nombre, delimiter="\t")
 	return matriz
-
-
-def generaGruposConCalendarioConEquipos(equipos):
 	
-	calendarios = []
-	print("Equipos")
-	print(equipos)
+def equiposRepetido(grupo):
+	ciudades = set()
 	
-	calendario = list(itertools.permutations(equipos.__iter__(),4))
-	print("Calendarios")
-	print(str(len(calendario)))
-	
-	
-	for equipos_ordenados in calendario:
-		print(equipos_ordenados)
-		calendarios.append(Grupo(equipos_ordenados))
-					
-	print("Un total de " +str(len(calendario)) +" calendarios unicos")
-	"""
-	print(list(calendarios)[0:5])
-	"""
-	return list(calendarios)
+	for equipo in grupo:
+		ciudades.add(equipo.ciudad)
+	return  not(len(ciudades) == len(grupo))
 	
 def main():
-	equipos,costes = cargaDatos()
+	
+	equipos,costes = cargaDatos(sys.argv[1],sys.argv[2])
 
+	grupos = [equipos[0],
+	equipos[1],
+	equipos[2],
+	equipos[3],
+	equipos[5],
+	equipos[6],
+	equipos[7],
+	equipos[9]]
+	
+	gruposPosibles = []
+	
+	for posi in itertools.permutations(grupos,4):
+		grupoA = list(posi)
+		grupoB = [x for x in grupos if x not in grupoA]
+		grupoA.append(equipos[4])
+		
+		grupoB.append(equipos[8])
+	
+		print("GrupoA")
+		print(grupoA)
+		print("GrupoB")
+		print(grupoB)
+		
+		combinationsA = itertools.permutations(grupoA,5)
+		combinationsB = itertools.permutations(grupoB,5)
+		for combinationA in combinationsA:
+			for combinationB in combinationsB:
+				print(combinationA)
+				print(combinationB)
+				gruposPosibles.append([combinationA,combinationB])
+	
+	
+	
 	i = 0
 	
-	grupos = generaGruposConCalendarioConEquipos(list(equipos));
 	
-	print(str(len(grupos)))
 	"""print(grupos);
 	"""
-		
-	calendarios = Calendario(grupos,costes)			
-	calendarios.generar_calendario()
-	i = i+1
+	calendarios = []
+	for grupo in gruposPosibles:	
+		print("calendario numero" + str(i))
+		calendario = Calendario(grupo,costes)			
+		calendario.generar_calendario()
+		calendarios.append(calendario)
+		i = i+1
 
-	calendarios.grupos.sort(key=lambda x: x.desviacion)
+	calendarios.sort(key=lambda x: x.total)
 
-	workbook = xlsxwriter.Workbook('desviacion.xlsx')	
+	workbook = xlsxwriter.Workbook('total.xlsx')	
 		
 	i = 1
-	for calendario in calendarios.grupos[:500]:
+	for calendario in calendarios[:500]:
 		nombre = 'Opcion '+ str(i)
 		
 		worksheet = workbook.add_worksheet(nombre)
